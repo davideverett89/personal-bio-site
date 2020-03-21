3724775704ba94179b9aa9cc56546878b0f52b33
David Everett <davideverett1989@gmail.com>
2020-03-21 17:23:52 -0500

    Screenshots needed.

--- src/javascripts/components/projectCards.js
+++ src/javascripts/components/projectCards.js
@@ -6,14 +6,18 @@ const createProjectCards = () => {
   let domString = '';
   projects.forEach((project) => {
     if (project.available) {
-      domString += '<div class="my-card">';
+      domString += '<div class="card-divider col-4">';
+      domString += '<div class="my-card d-flex flex-column justify-content-between align-items-center">';
       domString += `<img class="my-img" src="${project.screenshot}" alt="${project.title}">`;
-      domString += '<div class="my-container">';
-      domString += `<h4><b>${project.title}</b></h4>`;
+      domString += '<div class="my-content">';
+      domString += `<h5><b>${project.title}</b></h5>`;
       domString += `<p class="flex-grow-1">${project.description}</p>`;
       domString += `<p>Technologies Used: ${project.technologiesUsed}</p>`;
+      domString += '<div class="d-flex justify-content-center align-items-center">';
       domString += `<p><a href="${project.url}">Project Link</a></p>`;
       domString += `<p><a href="${project.githubUrl}">GitHub Link</a></p>`;
+      domString += '</div>';
+      domString += '</div>';
       domString += '</div>';
       domString += '</div>';
     }
