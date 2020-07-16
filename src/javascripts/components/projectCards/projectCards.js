7ea98287efa8bdeea85211be8dc33112f3cea632
David Everett <davideverett1989@gmail.com>
2020-07-15 22:27:20 -0500

    Moved about me over main image, changed card sizes, brought back hover events, changed footer color and made flex-row.

--- src/javascripts/components/projectCards/projectCards.js
+++ src/javascripts/components/projectCards/projectCards.js
@@ -8,11 +8,13 @@ const createProjectCards = () => {
       let domString = '';
       projects.forEach((project) => {
         if (project.available) {
-          domString += '<div class="card-divider col-6">';
-          domString += `  <div id="${project.id}" class="bg-dark text-white my-card p-3 d-flex flex-column justify-content-between align-items-center">`;
+          domString += '<div class="card-divider col-4">';
+          domString += `  <div id="${project.id}" class="bg-dark text-center text-white my-card pt-1 d-flex flex-column justify-content-between align-items-center">`;
           domString += `    <h5 class="mt-1"><b>${project.title}</b></h5>`;
-          domString += '    <div class="d-flex flex-column justify-content-between align-items-center my-content flex-grow-1">';
-          domString += `      <img class="my-1 my-img" src="${project.screenshot}" alt="${project.title}">`;
+          domString += '    <div class="card-body class="d-flex flex-column justify-content-between align-items-center my-content flex-grow-1">';
+          domString += '      <div class="col-12">';
+          domString += `        <img class="my-1 my-img img-fluid" src="${project.screenshot}" alt="${project.title}">`;
+          domString += '      </div>';
           domString += `      <p class="description flex-grow-1 my-1">${project.description}</p>`;
           domString += `      <p><small>Technologies Used: ${project.technologiesUsed}</small></p>`;
           domString += '      <div class="my-2 d-flex justify-content-center align-items-center">';
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,17 +2,16 @@ import '../styles/main.scss';
 import 'bootstrap';
 import projectCards from './components/projectCards/projectCards';
 
-// const hoverEvents = () => {
-//   $('.tech').hide();
-//   $('#technologiesPage').hover(() => {
-//     $('.tech').toggle('slow');
-//     $('#tech-header').toggleClass('underline');
-//   });
-// };
+const hoverEvents = () => {
+  $('.tech').hide();
+  $('#technologiesPage').hover(() => {
+    $('.tech').toggle('slow');
+  });
+};
 
 const init = () => {
   projectCards.createProjectCards();
-  // hoverEvents();
+  hoverEvents();
 };
 
 init();
