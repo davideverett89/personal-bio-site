3dc1cd84abf2e125a2665fdbb28b1e9ff95df391
David Everett <davideverett1989@gmail.com>
2020-07-17 12:26:15 -0500

    Redeployed to a better URL, tweaked project card styling, and tweaked footer styling.

--- src/javascripts/components/projectCards/projectCards.js
+++ src/javascripts/components/projectCards/projectCards.js
@@ -9,10 +9,10 @@ const createProjectCards = () => {
       projects.forEach((project) => {
         if (project.available) {
           domString += '<div class="card-divider col-4">';
-          domString += `  <div id="${project.id}" class="bg-dark text-center text-white my-card pt-1 d-flex flex-column justify-content-between align-items-center">`;
+          domString += `  <div id="${project.id}" class="bg-dark text-center text-white my-card pt-1 d-flex flex-column justify-content-center align-items-center">`;
           domString += `    <h5 class="mt-1"><b>${project.title}</b></h5>`;
-          domString += '    <div class="card-body class="d-flex flex-column justify-content-between align-items-center my-content flex-grow-1">';
-          domString += '      <div class="col-12">';
+          domString += '    <div class="card-body d-flex flex-column justify-content-between align-items-center my-content flex-grow-1">';
+          domString += '      <div class="px-3">';
           domString += `        <img class="my-1 my-img img-fluid" src="${project.screenshot}" alt="${project.title}">`;
           domString += '      </div>';
           domString += `      <p class="description flex-grow-1 my-1">${project.description}</p>`;
