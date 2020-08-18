a159e16919fc77f3b1e0bb7a4ad557cbde7bcfe7
David Everett <davideverett1989@gmail.com>
2020-08-17 21:16:54 -0500

    Looking good.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,16 +2,16 @@ import '../styles/main.scss';
 import 'bootstrap';
 import projectCards from './components/projectCards/projectCards';
 
-const hoverEvents = () => {
-  // $('.tech').hide();
-  // $('#technologiesPage').hover(() => {
-  //   $('.tech').toggle('slow');
-  // });
-};
+// const hoverEvents = () => {
+//   $('.tech').hide();
+//   $('#technologiesPage').hover(() => {
+//     $('.tech').toggle('slow');
+//   });
+// };
 
 const init = () => {
   projectCards.createProjectCards();
-  hoverEvents();
+  // hoverEvents();
 };
 
 init();
