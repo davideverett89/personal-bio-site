6aea83dc9198857dbd0d1545241280714118c4a3
David Everett <davideverett1989@gmail.com>
2020-10-06 23:52:13 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -9,9 +9,28 @@ import projectCards from './components/projectCards/projectCards';
 //   });
 // };
 
+const scrollEvents = () => {
+  const checkScroll = () => {
+    const startY = $('#nav-main').height() * 2;
+
+    if ($(window).scrollTop() > startY) {
+      $('#nav-main').addClass('scrolled');
+    } else {
+      $('#nav-main').removeClass('scrolled');
+    }
+  };
+
+  if ($('#nav-main').length > 0) {
+    $(window).on('scroll load resize', () => {
+      checkScroll();
+    });
+  }
+};
+
 const init = () => {
   projectCards.createProjectCards();
   // hoverEvents();
+  scrollEvents();
 };
 
 init();
