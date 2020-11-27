2f63ea068ca822dde42b1a4e0537519559b240a6
David Everett <davideverett1989@gmail.com>
2020-11-26 20:03:49 -0600

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -9,6 +9,26 @@ import projectCards from './components/projectCards/projectCards';
 //   });
 // };
 
+const navbarClickEvents = () => {
+  $('body').on('click', '#navToBio', () => {
+    $('#techSection').addClass('hide');
+    $('#projectSection').addClass('hide');
+    $('#bioSection').removeClass('hide');
+  });
+
+  $('body').on('click', '#navToTechnologies', () => {
+    $('#bioSection').addClass('hide');
+    $('#projectSection').addClass('hide');
+    $('#techSection').removeClass('hide');
+  });
+
+  $('body').on('click', '#navToProjects', () => {
+    $('#bioSection').addClass('hide');
+    $('#techSection').addClass('hide');
+    $('#projectSection').removeClass('hide');
+  });
+};
+
 const scrollEvents = () => {
   const checkScroll = () => {
     const startY = $('#nav-main').height() * 2;
@@ -28,9 +48,11 @@ const scrollEvents = () => {
 };
 
 const init = () => {
+  $('#bioSection').hide();
   projectCards.createProjectCards();
   // hoverEvents();
   scrollEvents();
+  navbarClickEvents();
 };
 
 init();
