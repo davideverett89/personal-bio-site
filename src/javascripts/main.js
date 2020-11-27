53e18716324d9067e4fc148678ba84304714e064
David Everett <davideverett1989@gmail.com>
2020-11-27 00:24:24 -0600

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,59 +2,28 @@ import '../styles/main.scss';
 import 'bootstrap';
 import projectCards from './components/projectCards/projectCards';
 
-// const hoverEvents = () => {
-//   $('.tech').hide();
-//   $('#technologiesPage').hover(() => {
-//     $('.tech').toggle('slow');
-//   });
-// };
-
 const navbarClickEvents = () => {
   $('body').on('click', '#navToBio', () => {
     $('#techSection').addClass('hide');
     $('#projectSection').addClass('hide');
     $('#bioSection').removeClass('hide');
-    // $('.content').removeClass('hide');
   });
 
   $('body').on('click', '#navToTechnologies', () => {
     $('#bioSection').addClass('hide');
     $('#projectSection').addClass('hide');
     $('#techSection').removeClass('hide');
-    // $('.content').removeClass('hide');
   });
 
   $('body').on('click', '#navToProjects', () => {
     $('#bioSection').addClass('hide');
     $('#techSection').addClass('hide');
-    // $('.content').addClass('hide');
     $('#projectSection').removeClass('hide');
   });
 };
 
-const scrollEvents = () => {
-  const checkScroll = () => {
-    const startY = $('#nav-main').height() * 2;
-
-    if ($(window).scrollTop() > startY) {
-      $('#nav-main').addClass('scrolled');
-    } else {
-      $('#nav-main').removeClass('scrolled');
-    }
-  };
-
-  if ($('#nav-main').length > 0) {
-    $(window).on('scroll load resize', () => {
-      checkScroll();
-    });
-  }
-};
-
 const init = () => {
-  $('#bioSection').hide();
   projectCards.createProjectCards();
-  // hoverEvents();
-  scrollEvents();
   navbarClickEvents();
 };
 
